a80b41f85fb064f15232d4b8d2f69d6da6486ffa
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2025-02-03 17:43:14 +0000

    Added more functionality

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const progress = document.getElementById("progress");
-console.log(progress);
-let amount = 0;
+const messages = ["Downloading ...", "Loading ...", "Installing ...", "Updating ...", "Configuring ..."];
 
-setInterval(() => {
+let amount = 0;
+let speed = Math.floor(Math.random() * 10 + 1);
+
+document.getElementById("button").addEventListener("click", cancelClicked);
+
+let id = setInterval(() => {
     progress.style.width = amount + "%";
     if (Math.random() < 0.25)
-        amount += 1;
+        amount += speed;
     if (amount >= 100){
-        document.getElementById("message").innerHTML = "Installing ...";
+        document.getElementById("message").innerHTML = messages[Math.floor(Math.random() * messages.length)];
         progress.style.width = "0px";
         amount = 0;
+        speed = Math.floor(Math.random() * 10 + 1);
     }
 }, 200);
+
+function cancelClicked(event) {
+    clearInterval(id);
+    progress.style.width = "95%";
+    document.getElementById("message").innerHTML = "Canceling ...";
+    document.body.style.cursor = "wait";
+    document.getElementById("button").style.cursor = "wait";
+}
